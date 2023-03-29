5fdb3620908de24a2cbc7d4254939646dde41485
Deepika GP <deepika@netstager.in>
2023-03-29 18:15:37 +0530

    My first fruitful commit

---
+++ # git.sh
@@ -0,0 +1,8 @@
+# git
+# centrlsed vs distributed
+git add file 1
+git commit -m 
+echo hello > file1.txt
+echo hello > file2.txt
+git commit -m "This is my initial commit"
+echo howryou >> file3.txt
