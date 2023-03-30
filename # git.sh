9128f8a816a2ba8e0963ebf52b89729d6372dca0
Deepika GP <deepika@netstager.in>
2023-03-30 11:07:16 +0530

    My second commit

--- # git.sh
+++ # git.sh
@@ -6,3 +6,12 @@ echo hello > file1.txt
 echo hello > file2.txt
 git commit -m "This is my initial commit"
 echo howryou >> file3.txt
+#cat
+
+yum update -y
+yum install -y
+which git
+git -version
+git config --global user.name "Naveen A"
+git config --global user.email naveenachandran35@gmail.com
+
