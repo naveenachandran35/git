2761c82a8de16a0c5a491df0fde19fc357c1b1f9
Deepika GP <deepika@netstager.in>
2023-03-30 15:22:08 +0530

    My first commit for naveen.txt file

--- # git.sh
+++ # git.sh
@@ -14,4 +14,35 @@ which git
 git -version
 git config --global user.name "Naveen A"
 git config --global user.email naveenachandran35@gmail.com
+mkdir mumbaigit
+cd mumbaigit
+git init
+cat >Mumbai1
+cat Mumabi1
+sudo nano Mumabi1
+SARE JAHAN SE ACHA
+git status # (if it is red, it means u did not add it)
+git add .
+git status
+git commit -m "first commit from Mumbai"
+git status
+git log
+git remote add origin https://github.com/naveen
+git push -u origin master
+vi .gitignore
+- css
+- java
+git add .gitignore
+git commit -m "ignore file format"
+git status
+touch file1.txt
+git log --grep "xyz"
+
+# Git Branches
+
+cd mumbaigit
+git log --oneline
+git branch
+
+git commit -m "names.txt"    
 
